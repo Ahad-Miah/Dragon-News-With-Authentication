80de7b8e7de06599444ed85292e9d18177f38ec8
Ahad-Miah <ahadahmedcc@gmail.com>
2024-11-14 11:50:47 +0600

    basic project setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Career from "../Pages/Career/Career";
+
+
+const Router =createBrowserRouter([
+        {
+            path:'/',
+            element: <Home></Home>
+        },
+        {
+            path:'about',
+            element:<About></About>
+        },
+        {
+            path:'career',
+            element:<Career></Career>
+        }
+
+])
+   
+
+export default Router;
